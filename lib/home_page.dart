74d24899e61971b1da0b4583a7eb7e0eff7fe655
Hadjimamas <andreashadjimama@hotmail.com>
2023-08-25 12:20:44 +0300

    Futbol Web

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:futbol_web_page/platform_details.dart';
 import 'package:universal_platform/universal_platform.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -13,12 +15,16 @@ class HomePage extends StatefulWidget {
 class HomePgeState extends State<HomePage> {
   @override
   void initState() {
-    if (UniversalPlatform.isAndroid) {
+    if ((PlatformDetails().isMobile || PlatformDetails().isDesktop) &&
+        UniversalPlatform.isAndroid) {
       launchUrlString(
           'https://play.google.com/store/apps/details?id=com.livescore.cyprus');
-    } else if (UniversalPlatform.isIOS) {
-      launchUrlString(
-          'https://apps.apple.com/cy/app/futbol-cyprus-livescore/id6449159778?platform=iphone');
+    } else if ((PlatformDetails().isMobile || PlatformDetails().isDesktop) &&
+        UniversalPlatform.isIOS) {
+      launchUrlString('https://apps.apple.com/app/id6449159778');
+    } else if ((PlatformDetails().isMobile || PlatformDetails().isDesktop) &&
+        UniversalPlatform.isWindows) {
+      print('Windows Platform');
     }
     super.initState();
   }
@@ -26,102 +32,105 @@ class HomePgeState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        persistentFooterButtons: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Follow Us',
-                style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15.0),
+      persistentFooterButtons: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Follow Us',
+              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 15.0),
+            ),
+            IconButton(
+              tooltip: 'Facebook',
+              onPressed: () {
+                launchUrlString(
+                    'https://www.facebook.com/profile.php?id=100094163409496');
+              },
+              icon: const Icon(
+                Icons.facebook,
+                color: Colors.lightBlue,
+                size: 20.0,
               ),
-              IconButton(
-                tooltip: 'Facebook',
-                onPressed: () {
-                  launchUrlString(
-                      'https://www.facebook.com/profile.php?id=100094163409496');
-                },
-                icon: const Icon(
-                  Icons.facebook,
-                  color: Colors.lightBlue,
-                  size: 20.0,
+            ),
+            // IconButton(
+            //   tooltip: 'GitHub',
+            //   onPressed: () {
+            //     launchUrlString('https://github.com/hadjimamas');
+            //   },
+            //   icon: const Icon(
+            //     FontAwesomeIcons.github,
+            //     color: Colors.grey,
+            //     size: 20.0,
+            //   ),
+            // ),
+            IconButton(
+              tooltip: 'Privacy Policy',
+              onPressed: () {
+                launchUrlString(
+                    'https://github.com/Hadjimamas/Privacy_Policy_Futbol/blob/main/PrivacyPolicy.md');
+              },
+              icon: const Icon(Icons.privacy_tip_outlined,
+                  color: Colors.grey, size: 20.0),
+            ),
+          ],
+        ),
+      ],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Stack(
+                  children: [
+                    Image.asset('assets/home_page.png', scale: 5),
+                    Container(
+                      padding: const EdgeInsets.only(left: 100),
+                      child: Image.asset('assets/standings.png', scale: 5),
+                    ),
+                  ],
                 ),
-              ),
-              IconButton(
-                tooltip: 'Instagram',
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.instagram,
-                  color: Colors.pinkAccent,
-                  size: 20.0,
+              ],
+            ),
+            Column(
+              children: [
+                const SelectableText(
+                  'Futbol - Cyprus Livescore',
+                  style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-              IconButton(
-                tooltip: 'Privacy Policy',
-                onPressed: () {
-                  launchUrlString(
-                      'https://github.com/Hadjimamas/Privacy_Policy_Futbol/blob/main/PrivacyPolicy.md');
-                },
-                icon: const Icon(Icons.privacy_tip_outlined,
-                    color: Colors.grey, size: 20.0),
-              ),
-            ],
-          ),
-        ],
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Stack(
-                    children: [
-                      Image.asset('assets/home_page.png', scale: 5),
-                      Container(
-                        padding: const EdgeInsets.only(left: 100),
-                        child: Image.asset('assets/standings.png', scale: 5),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const SelectableText(
-                    'Futbol - Cyprus Livescore',
-                    style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const Text(
-                    'DOWNLOAD NOW!',
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          launchUrlString(
-                              'https://play.google.com/store/apps/details?id=com.livescore.cyprus');
-                        },
-                        child:
-                            Image.asset('assets/getOnGooglePlay.png', scale: 3),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          launchUrlString(
-                              'https://apps.apple.com/app/id6449159778');
-                        },
-                        child: Image.asset('assets/getOniOS.png', scale: 5),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+                const Text(
+                  'DOWNLOAD NOW!',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        launchUrlString(
+                            'https://play.google.com/store/apps/details?id=com.livescore.cyprus');
+                      },
+                      child:
+                          Image.asset('assets/getOnGooglePlay.png', scale: 3),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        launchUrlString(
+                            'https://apps.apple.com/app/id6449159778');
+                      },
+                      child: Image.asset('assets/getOniOS.png', scale: 5),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
